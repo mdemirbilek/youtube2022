9f0593a55aebf02f165b23dcd1a848672fcdd592
mdemirbilek <mdemirbilek@gmail.com>
2023-11-24 20:25:09 +0100

    initti

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import { createBrowserRouter, RouterProvider, Route, Outlet} from 'react-router-dom'
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Product from './pages/Product/Product';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+const Layout = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children:[
+      {
+        path: "/",
+        element: <Home />
+      },  {
+        path: "/products/:id",
+        element: <Products />
+      },  {
+        path: "/product/:id",
+        element: <Product />
+      },  {
+        path: "/blog",
+        element: <span>Blog</span>
+      },
+    ]
+  }
+
+])
+
 function App() {
-  return <div>Hello World!</div>;
+  return <div>
+    <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
